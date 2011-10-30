a99dcf499a09a281526a7666a62c0f8330b32022
zenja <zenja@localhost.localdomain>
2011-10-30 14:08:59 -0600

    Initializing

---
+++ main.cpp
@@ -0,0 +1,19 @@
+/* 
+ * File:   main.cpp
+ * Author: zenja
+ *
+ * Created on October 30, 2011, 2:00 PM
+ */
+
+#include <cstdlib>
+
+using namespace std;
+
+/*
+ * 
+ */
+int main(int argc, char** argv) {
+
+    return 0;
+}
+
